79dc71bbb6a256166ea5915ec5c3efebeffd0607
Samuel Ogbodo <samuelogbodo515@gmail.com>
2025-02-22 23:41:16 +0100

    update

--- newchatbot.js
+++ newchatbot.js
@@ -318,7 +318,9 @@
 
         messagesContainer.appendChild(message);
         messagesContainer.scrollTop = messagesContainer.scrollHeight;
-        focusInput();
+        if (window.innerWidth >= 768) { // Check screen size
+            focusInput();
+        }
     }
 
     function showTypingIndicator() {
@@ -379,7 +381,10 @@
             <line x1="12" y1="5" x2="12" y2="19"></line>
             <polyline points="19 12 12 19 5 12"></polyline>
           </svg>`
-            : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg>`;
+            : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg>`;
+            if (window.innerWidth >= 768) {
+                focusInput();
+            }
     });
 
     sendButton.addEventListener('click', handleSendMessage);
@@ -399,6 +404,8 @@
     // Move initial message inside the messages container
     messagesContainer.appendChild(initialMessage);
 
-    focusInput()
+    if (window.innerWidth >= 768) {
+        focusInput();
+    }
 
 })();
